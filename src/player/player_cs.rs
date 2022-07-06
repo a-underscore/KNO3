b52faaaa8802406ae6f860e3aa17bdf0db831eef
Brendan <brendansbortz@protonmail.com>
2022-07-06 17:21:32 -0400

    better

--- src/player/player_cs.rs
+++ src/player/player_cs.rs
@@ -17,10 +17,10 @@ impl PlayerCS {
         let mouse_position = viewport.get_mouse_position();
         let scale = owner.scale();
 
-        if mouse_position.x < flip_point {
-            owner.set_scale(Vector2::new(-1.0, scale.y));
-        } else if mouse_position.x > flip_point {
-            owner.set_scale(Vector2::new(1.0, scale.y));
+        if (mouse_position.x < flip_point && scale.x > 0.0)
+            || (mouse_position.x > flip_point && scale.x < 0.0)
+        {
+            owner.set_scale(Vector2::new(scale.x * -1.0, scale.y));
         }
     }
 }
