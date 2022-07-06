26f6eafc78fce7f1240ba15f349529fef89d8140
Brendan <brendansbortz@protonmail.com>
2022-07-06 16:10:14 -0400

    better

---
+++ src/player/player_cs.rs
@@ -0,0 +1,26 @@
+use gdnative::{api::CollisionShape2D, prelude::*};
+
+#[derive(NativeClass)]
+#[inherit(CollisionShape2D)]
+pub struct PlayerCS;
+
+#[methods]
+impl PlayerCS {
+    fn new(_owner: &CollisionShape2D) -> Self {
+        Self
+    }
+
+    #[export]
+    fn _physics_process(&mut self, owner: &CollisionShape2D, _delta: f64) {
+        let viewport = unsafe { owner.get_viewport().unwrap().assume_safe() };
+        let flip_point = viewport.size().x / 2.0;
+        let mouse_position = viewport.get_mouse_position();
+        let scale = owner.scale();
+
+        if mouse_position.x < flip_point {
+            owner.set_scale(Vector2::new(-1.0, scale.y));
+        } else if mouse_position.x > flip_point {
+            owner.set_scale(Vector2::new(1.0, scale.y));
+        }
+    }
+}
