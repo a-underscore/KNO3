47f411182a4f6608dc051d0ec9dfee6c5d1166cc
Brendan <brendansbortz@protonmail.com>
2022-07-11 19:21:02 -0400

    better

--- src/player/player_body.rs
+++ src/player/player_body.rs
@@ -6,7 +6,7 @@ const VELOCITY_SCALE: f32 = 100.0;
 const MOVE_SPEED: Vector2 = Vector2::new(10.0, 20.0);
 const JUMP_DECAY: f32 = 1.5;
 const DASH_MODIFIER: f32 = 25.0;
-const DASH_TIME: Duration = Duration::from_millis(100);
+const DASH_TIME: Duration = Duration::from_millis(1000);
 
 #[derive(NativeClass)]
 #[inherit(KinematicBody2D)]
@@ -48,19 +48,11 @@ impl PlayerBody {
         if input.is_action_pressed("player_right", false) {
             self.velocity.x = MOVE_SPEED.x;
 
-            if input.is_action_just_pressed("player_dash", false)
-                && Instant::now().duration_since(self.dash_time) >= DASH_TIME
-            {
-                self.velocity.x += DASH_MODIFIER * MOVE_SPEED.x;
-            }
+            self.attempt_dash(&input, 1.0);
         } else if input.is_action_pressed("player_left", false) {
             self.velocity.x = -MOVE_SPEED.x;
 
-            if input.is_action_just_pressed("player_dash", false)
-                && Instant::now().duration_since(self.dash_time) >= DASH_TIME
-            {
-                self.velocity.x -= DASH_MODIFIER * MOVE_SPEED.x;
-            }
+            self.attempt_dash(&input, -1.0);
         } else {
             self.velocity.x = 0.0;
         }
@@ -71,6 +63,15 @@ impl PlayerBody {
             self.velocity.y += JUMP_DECAY;
         } else {
             self.velocity.y = GRAVITY;
+        }
+    }
+
+    fn attempt_dash(&mut self, input: &Input, modifier: f32) {
+        if input.is_action_just_pressed("player_dash", false)
+            && Instant::now().duration_since(self.dash_time) >= DASH_TIME
+        {
+            self.velocity.x += DASH_MODIFIER * MOVE_SPEED.x * modifier;
+            self.dash_time = Instant::now();
         }
     }
 }
